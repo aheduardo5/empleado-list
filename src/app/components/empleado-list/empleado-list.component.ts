3a4f7bacb32ce4a7efc940cc457fc614630bff8a
Jose Aguilar <jose.a.aguilar@accenture.com>
2022-11-17 11:18:10 -0600

    list empleados has been displayed and empleado.ts has been created

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/empleado';
 
 @Component({
   selector: 'app-empleado-list',
   templateUrl: './empleado-list.component.html',
-  styleUrls: ['./empleado-list.component.css']
+  styleUrls: ['./empleado-list.component.css'],
 })
 export class EmpleadoListComponent implements OnInit {
+  listEmpleados: Empleado[] = [
+    {
+      legajo: 1,
+      nombre: 'Juan',
+      apellido: 'Esparza',
+      sexo: 'Masculino',
+      salario: 23005,
+    },
+    {
+      legajo: 2,
+      nombre: 'Peter',
+      apellido: 'Parker',
+      sexo: 'Masculino',
+      salario: 32452,
+    },
+    {
+      legajo: 3,
+      nombre: 'Marco',
+      apellido: 'Antonio',
+      sexo: 'Masculino',
+      salario: 45231,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
